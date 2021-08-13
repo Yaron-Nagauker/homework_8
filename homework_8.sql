c99867eda2a3bca9c45dd21337dc486c3b2d5989
Yaron-Nagauker <76478484+Yaron-Nagauker@users.noreply.github.com>
2021-08-13 04:03:53 +0300

    Create homework_8.sql

---
+++ homework_8.sql
@@ -0,0 +1,20 @@
+// homework 8
+
+create or replace FUNCTION  sp_statistics(x double precision, y double precision ,OUT the_mul double precision,
+     OUT the_div double precision, OUT sqrt_x double precision, OUT sqrt_y double precision, 
+     OUT power_x_y double precision) 
+
+LANGUAGE 'plpgsql' AS
+    $$
+        BEGIN
+            the_mul:=x*y;
+			the_div:=x/y;
+			sqrt_x:=(select * from sqrt(x));
+			sqrt_y:=(select * from sqrt(y));
+			power_x_y:=(select * from power(x, y));
+
+
+        END;
+    $$;
+
+select * from sp_statistics(9,16);
