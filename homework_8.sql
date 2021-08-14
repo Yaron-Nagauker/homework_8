dff1ff0451f0678384d881ccc84c59c83b2d369a
Yaron-Nagauker <76478484+Yaron-Nagauker@users.noreply.github.com>
2021-08-14 16:39:42 +0300

    Update homework_8.sql

--- homework_8.sql
+++ homework_8.sql
@@ -7,11 +7,11 @@ create or replace FUNCTION  sp_statistics(x double precision, y double precision
 LANGUAGE 'plpgsql' AS
     $$
         BEGIN
-            the_mul:=x*y;
-			the_div:=x/y;
-			sqrt_x:=(select * from sqrt(x));
-			sqrt_y:=(select * from sqrt(y));
-			power_x_y:=(select * from power(x, y));
+            	the_mul:=x*y;
+		the_div:=x/y;
+		sqrt_x:=(select * from sqrt(x));
+		sqrt_y:=(select * from sqrt(y));
+		power_x_y:=(select * from power(x, y));
 
 
         END;
